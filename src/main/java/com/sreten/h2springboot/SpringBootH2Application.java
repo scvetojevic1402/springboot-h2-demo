b6a040c010dc8bc497a1417aa53f20ccda0c7f47
Sreten <scvetojevic1402@gmail.com>
2020-05-25 18:45:02 -0400

    h2 server mixed mode added

--- src/main/java/com/sreten/h2springboot/SpringBootH2Application.java
+++ src/main/java/com/sreten/h2springboot/SpringBootH2Application.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
+//compile and build: mvn clean install
+//double click on jar file in target folder to run
+//jps to find the process id
+//TASKKILL /PID %PID% /F
+
 @SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class SpringBootH2Application {
 
